bda83089aa23da98e3ed4c6b617ede298bcf0522
aman <aman@ginilytics.com>
2024-04-29 15:10:30 +0530

    rest code

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -16,8 +17,9 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      // href="/dashboard/invoices"
       className="rounded-md border p-2 hover:bg-gray-100"
+      href={`/dashboard/invoices/${id}/edit`}
     >
       <PencilIcon className="w-5" />
     </Link>
@@ -34,3 +36,5 @@ export function DeleteInvoice({ id }: { id: string }) {
     </>
   );
 }
+
+
