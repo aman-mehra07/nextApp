11993b1c0c3ded336f0d50f559631897c644beb5
aman <aman@ginilytics.com>
2024-04-29 15:11:50 +0530

    teset

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,7 +156,6 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
-  console.log(invoice); 
   try {
     const data = await sql<InvoiceForm>`
       SELECT
